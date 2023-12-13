a3730d04e8f2760efcdf422c6b79d78b848b38ae
Sharma-Aditya7 <aditya71104@gmail.com>
2023-12-13 12:54:27 +0530

    CHANGE: Trying to overlay on a background ISSUE:  Getting size.width and size.height>0 error INFERENCE: Will probably have to add a configurator to accomodate differences in Capture hardware

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,17 +1,25 @@
 import cv2
-import mediapipe
 import cvzone
 
 
 cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+#   ====================================================================================================
+#   The parameters will have to be re-tuned when overlaying on the final bg
+#   ====================================================================================================
 
 
 while True:
     imgBG = cv2.imread("Resources/BG.png")
-
     success, img = cap.read()
+
+    imgScaled = cv2.resize(img,(0,0),None,0.875,0.875)      #Using it to resize the image on a scale
+
     cv2.imshow("Image",img)
     cv2.imshow("BG", imgBG)
+    cv2.imshow("Scaled", imgScaled)
 
     cv2.waitKey(1)
 
